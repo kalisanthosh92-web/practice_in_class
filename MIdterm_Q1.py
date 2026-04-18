c28101526249bd5ce814bf12b90f7f340cc57075
kalisanthosh92-web <kalisanthosh92@gmail.com>
2026-04-18 10:08:31 +0800

    do it now

---
+++ MIdterm_Q1.py
@@ -0,0 +1,92 @@
+
+IGN = input("In Game Name (IGN): ")
+Rank = input("Current Rank: ")
+Hero = ['Layla','Tigreal','Gusion','Kagura','Chou']
+roles =['Marksman','Tank','Assassin','Mage','Fighter']
+  
+print()
+   
+
+
+print('=' * 50)
+print(f'{'MOBILE LEGENDS --HERO ROSTER':^50}')
+print('='*50)
+for i in range(1,6):
+    print(f'| {i} | {Hero[i-1]:<29} | {roles[i-1]:^10} |')
+print('='*50)
+
+matches_played = 0
+wins = 0
+loss = 0
+for j in range(1,5):
+    print(f'---MATCH {j}')
+    hero_number = int(input('Hero number (0 to skip): '))
+    if hero_number > 0 :
+        kills = int(input('Kills: '))
+        Deaths = int(input('Deaths: '))
+        Assists = int(input('Assists: '))
+        Result = input('Result(W/L): ').lower()
+        if Result == 'w':
+            match_result = 'Win'
+            wins += 1
+        else :
+            match_result = 'Loss'
+            loss += 1 
+
+        while Deaths == 0:
+            KDA = (kills + Assists)/1
+        else : 
+            KDA = (kills + Assists)/Deaths 
+
+        if KDA >= 5 and Result == 'w':
+            tag = 'DOMINATION!'
+        elif KDA >= 5 and Result == 'l':
+            tag = "Carried Hard"
+        elif KDA < 5 and Result == 'w':
+            tag = 'Team Effort'
+        elif KDA < 5 and Result == 'l':
+            tag = 'Better Luck Next Game'
+        matches_played += 1
+        break
+
+      
+
+    else :
+        continue
+print('=' * 50)
+print(f"{str(IGN) + '-- MATCH LOG(' + str(Rank) + ')':^50}")
+print('='*50)
+print(f'({matches_played}) {IGN:<10}| KDA: {KDA:.2f} | {match_result} | {tag}')
+
+
+
+print('-'*50)
+
+print('Matches Played : ',matches_played)
+print(f'Wins : {wins}   |  Losses : {loss}')
+win_rate = ((wins/matches_played)*100)
+print(f'Win Rate : {win_rate:.0f}')
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
