8e13618ae238050296226c1a0cbe491bb60c4c29
kalisanthosh92-web <kalisanthosh92@gmail.com>
2026-08-28 00:26:11 +0800

    done.

--- LA2_OOP.py
+++ LA2_OOP.py
@@ -2,20 +2,20 @@ class Phone:
     def __init__(self,brand, model):
         self.Brand = brand
         self.Model = model
-
+        print(brand + model)
 
     def describePhone(self):
         return '{}-{}.'.format(self.Brand, self.Model)
 
 
-#class Android(Phone):
- #   def __init__(self, brand, model):
-  #      super().__init__(brand, model)
+class Android(Phone):
+    def __init__(self, brand, model):
+        super().__init__(brand, model)
 
 
 And1 = Android('oppo','Reno 13')
 print(Android.describePhone(And1))
 
 
-Ph1= Phone('samsung,'Galaxy-z)
+Ph1= Phone ('samsung','Galaxy-z')
 print(Phone.describePhone(Ph1))
